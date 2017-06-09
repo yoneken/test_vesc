166027e9311e72163c8dc23c6b8097682c41d148
Kenta Yonekura <yoneken@ieee.org>
2017-06-09 15:51:15 +0900

    Edit Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # test_vesc
+
+Don't forget to register the local ROS python library to your PYTHONPATH.
--- scripts/test_vesc.py
+++ scripts/test_vesc.py
@@ -10,21 +10,25 @@ def callback(data):
 
 sense_topic = rospy.get_param('sense_topic', 'sensors/core')
 duty_topic = rospy.get_param('duty_topic', 'commands/motor/duty_cycle')
+cur_topic = rospy.get_param('current_topic', 'commands/motor/current')
 rpm_topic = rospy.get_param('rpm_topic', 'commands/motor/speed')
 
 print sense_topic, duty_topic, rpm_topic
 
 pub_duty = rospy.Publisher(duty_topic, Float64, queue_size=10)
 pub_rpm = rospy.Publisher(rpm_topic, Float64, queue_size=10)
+pub_cur = rospy.Publisher(cur_topic, Float64, queue_size=10)
 rospy.Subscriber(sense_topic, VescStateStamped, callback)
 
 rospy.init_node('test_vesc')
 
 while not rospy.is_shutdown():
   #pub_duty.publish(1.0)
+  #pub_cur.publish(3.0)
   for i in range(10):
     print "i: " + str(i)
-    #pub_duty.publish(0.1*i)
-    pub_rpm.publish(10000*i)
+    pub_duty.publish(0.1*i)
+    #pub_cur.publish(0.5*i)
+    #pub_rpm.publish(10000*i)
     rospy.sleep(1.0)
 
